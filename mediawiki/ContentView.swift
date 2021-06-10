252355e6d4bd466943762652a49327d7990de0ca
takutoki <34600200+takutoki@users.noreply.github.com>
2021-06-10 21:11:51 +0900

    Initial Commit

---
+++ mediawiki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mediawiki
+//
+//  Created by takumitokiwa on 2021/06/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mediawiki/mediawikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mediawikiApp.swift
+//  mediawiki
+//
+//  Created by takumitokiwa on 2021/06/10.
+//
+
+import SwiftUI
+
+@main
+struct mediawikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
