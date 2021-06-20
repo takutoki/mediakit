dd2bf84a1c6d9343e8951be2faf6a468a1bd7da4
takutoki <34600200+takutoki@users.noreply.github.com>
2021-06-20 13:20:30 +0900

    wiki用のapiクラス作成

---
+++ mediaWiki/Utils/MediaWikiApi.swift
@@ -0,0 +1,22 @@
+//
+//  MediaWikiApi.swift
+//  mediawiki
+//
+//  Created by takumitokiwa on 2021/06/13.
+//
+
+import Alamofire
+import Combine
+
+class MediaWikiApi: BaseApiProtocol {
+
+    typealias ResponseData = WikiImageListResponse
+
+    var baseUrl: String = "https://ja.wikipedia.org/w/api.php"
+    var queryParameters = ""
+    var headers: HTTPHeaders? = [.accept("application/json")]
+
+    func hoge() {
+        get(url: <#T##String#>, parameters: <#T##[String : String]#>)
+    }
+}
